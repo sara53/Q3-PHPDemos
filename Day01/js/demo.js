52efdd89391fa000435ea2290ce3fb5b8c01f51e
sara <sara.atef001@gmail.com>
2024-05-23 11:48:38 +0300

    day01

---
+++ Day01/js/demo.js
@@ -0,0 +1,183 @@
+/**
+    Agenda
+    == JS History
+    == Variables
+ /** Datatype in js
+ * --------------------------
+ * = Primitive Datatype
+ *      -- String
+ *      -- number
+ *      -- boolean
+ *      -- undefined
+ *      -- null
+ * == Reference Datatype
+ *  -- Object
+ * -----------------------------
+ * 
+ * == falsy values
+ *
+ */
+
+// var fname = null;
+// console.log(typeof fname);
+// var fname = "PHP";
+// var fname = 344;
+// console.log(fname);
+// fname = 123;
+// fname = true;
+// console.log(fname);
+// console.log(typeof fname);
+
+// var myFlag;
+// myFlag = true;
+// console.log(myFlag);
+// console.log(typeof myFlag);
+// var myNum = 123;
+// var myNum2 = 123.256;
+// var flag = true;
+
+// console.log(typeof fname);
+// console.log(typeof myNum);
+// console.log(typeof myNum2);
+// console.log(typeof flag);
+
+// var fname = 1;
+// console.log(typeof fname);
+// var fname = String("faf");
+// console.log(fname);
+// console.log(typeof fname);
+// NaN --> Not a number
+// var num = 10;
+// var num = Number("");
+// console.log(num);
+// console.log(typeof num);
+/** falsy Values
+ * false
+ * 0
+ * ""
+ * undefined
+ * null
+ * NaN
+ *
+ */
+// var flag = true;
+// var flag = Boolean("dsfghgjh");
+// console.log(flag);
+/** Operator In JS
+ * == (+,-,*,/)
+ * [+] --> number + number --> add
+ *     --> string + string -->concat
+ *
+ * -- coercion --> js Engine convert automatic from datatype to anther datatype
+ *
+ */
+// var num1 = 10; // "10" + "20"
+
+// var num2 = "mona";
+
+// console.log(num1 - num2);
+// // console.log(num1 + num2);
+/**
+ * [==] values
+ * [===] values & datatype
+ *
+ */
+// 10 "10"
+
+// var x = true; // 1 -- number
+// var y = "true"; // NaN --> number
+
+// console.log(x == y);
+/** Logical Operator
+ * &&
+ * ||
+ * !
+ *
+ */
+
+/**
+ * &&
+ * true && true ==> true
+ * true && false ==> false
+ * false && true ==> false
+ * false && false ==> false
+ * --------------------------
+ * ||
+ * true || true --> true
+ * true || false --> true
+ * false || true --> true
+ * false || false --> false
+ *
+ * !
+ * --- True -> false
+ * false -> true
+ */
+
+/** Short Circuit
+ * "mona" && "ali"
+ * true &&
+ *
+ * "" && "alaa"
+ * false &&
+ *
+ * "ali" ||
+ * 0 || "ad"
+ * 0 || undefined
+ * "undefine" &&
+ */
+/**  */
+// var trackName = "d";
+// if (trackName == "php") {
+// 	// logic
+// 	console.log("Welcome Php");
+// } else {
+// 	console.log("Welcome from iti");
+// }
+
+// trackName == "php" ? console.log("PHP") : console.log("ITI");
+
+// if (trackName == "php") {
+// 	console.log("PHP");
+// } else if (trackName == "Mearn") {
+// 	console.log("Mearn");
+// } else {
+// 	console.log("ITI");
+// }
+
+// for (var i = 0; i < 5; i++) {
+// 	console.log(i);
+// }
+
+// var i = 6;
+// while (i < 5) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// do {
+// 	console.log(i);
+// 	i++;
+// } while (i < 5);
+/** */
+// var fname = 44;
+// console.log(isNaN(fname));
+/** */
+
+// var trackName = prompt("Enter Your Name", "ITI");
+// console.log(trackName);
+// var num = Number(prompt("enter a num"));
+// console.log(num + 20);
+
+// document.writeln("Welcome PHP");
+// var trackName = "MEarn";
+// document.writeln("<h1>Welcome " + trackName + "</h1>");
+
+// alert("file deleted");
+// var result = confirm("Are you want to delete this course");
+// result
+// 	? document.writeln("course deleted")
+// 	: document.writeln("Operation cancelled");
+/** self study
+ * regx in js
+ *
+ */
